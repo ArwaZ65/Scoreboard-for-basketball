f01fc96c6bbe3031ce9da815babd53accd959127
ArwaZ65 <113128746+ArwaZ65@users.noreply.github.com>
2024-02-04 04:42:29 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+let digital = 0
+let digital2 = 0
+function f1() {
+    digital += 1;
+    let clock = document.getElementById("zero");
+    console.log(clock);
+    clock.textContent = digital;
+}
+function f2() {
+    digital += 2;
+    let clock = document.getElementById("zero");
+    console.log(clock);
+    clock.textContent = digital;
+}
+function f3() {
+    digital += 3;
+    let clock = document.getElementById("zero");
+    console.log(clock);
+    clock.textContent = digital;
+}
+function f11() {
+    digital2 += 1;
+    let clock = document.getElementById("zero.1");
+    console.log(clock);
+    clock.textContent = digital2;
+}
+function f22() {
+    digital2 += 2;
+    let clock = document.getElementById("zero.1");
+    console.log(clock);
+    clock.textContent = digital2;
+}
+function f33() {
+    digital2 += 3;
+    let clock = document.getElementById("zero.1");
+    console.log(clock);
+    clock.textContent = digital2;
+}
